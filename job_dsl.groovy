91716d38963e84178d0b7369f9c717d65c1d6792
madhav411 <44801760+madhav411@users.noreply.github.com>
2020-03-04 13:08:39 +0530

    Update job_dsl.groovy

--- job_dsl.groovy
+++ job_dsl.groovy
@@ -1,6 +1,6 @@
-pipelineJob('k8s-deploy') {
+pipelineJob('k8s-deployment') {
 
-  def repo = 'https://github.com/madhav411/app-mono-helmstate.git'
+  def repo = 'https://github.com/madhav411/helm-values.git'
 
   triggers {
     githubPush()
@@ -28,31 +28,7 @@ pipelineJob('k8s-deploy') {
 
 pipelineJob('image-orchestrator-job') {
 
-  def repo = 'https://github.com/madhav411/app-mono-orchestrator.git'
-
-  description("Pipeline for $repo")
-
-  definition {
-    cpsScm {
-      lightweight(true)
-      scm {
-        git {
-          remote {
-            credentials('madhav_github_credentials')
-            url(repo)
-          }
-          branch('master')
-          scriptPath('Jenkinsfile')
-          extensions { }
-        }
-      }
-    }
-  }
-}
-
-pipelineJob('app-mono-build-job') {
-
-  def repo = 'https://github.com/madhav411/app-mono.git'
+  def repo = 'https://github.com/madhav411/docker-trigger.git'
 
   description("Pipeline for $repo")
 
